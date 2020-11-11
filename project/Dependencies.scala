fadadd71f50c81b62150c6a3a3e9480338d8bf43
Adrien Besnard <adrien.besnard@ultra.io>
2020-11-11 18:51:01 +0100

    Update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,19 +2,20 @@ import sbt._
 
 object Dependencies {
 
+  lazy val scalaTest = Seq(
+    "org.scalatest" %% "scalatest" % "3.1.3"
+  )
+
   lazy val cats = Seq(
-    "org.typelevel" %% "cats-core" % "2.1.0",
-    "org.typelevel" %% "cats-effect" % "2.0.0"
+    "org.typelevel" %% "cats-core" % "2.1.1",
+    "org.typelevel" %% "cats-effect" % "2.1.4",
+    "org.typelevel" %% "kittens" % "2.1.0",
+    "org.typelevel" %% "mouse" % "0.25"
   )
 
   lazy val fs2 = Seq(
-    "co.fs2" %% "fs2-core" % "2.1.0",
-    "co.fs2" %% "fs2-io" % "2.1.0"
-  )
-
-  lazy val scalaTest = Seq(
-    "org.scalactic" %% "scalactic" % "3.1.0",
-    "org.scalatest" %% "scalatest" % "3.1.0" % "test"
+    "co.fs2" %% "fs2-core" % "2.4.0",
+    "co.fs2" %% "fs2-io" % "2.4.0"
   )
 
   lazy val slf4j = Seq(
